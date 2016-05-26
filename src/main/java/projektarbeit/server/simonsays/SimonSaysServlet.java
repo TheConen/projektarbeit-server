18636ec0271e9fc1f2f4d186cd4fec32807ba02c
TheConen <theconen@gmail.com>
2016-05-27 00:30:39 +0200

    blub

--- src/main/java/projektarbeit/server/simonsays/SimonSaysServlet.java
+++ src/main/java/projektarbeit/server/simonsays/SimonSaysServlet.java
@@ -2,9 +2,6 @@ package projektarbeit.server.simonsays;
 
 import com.amazon.speech.speechlet.servlet.SpeechletServlet;
 
-/**
- * Created by johan on 20.05.2016.
- */
 public class SimonSaysServlet extends SpeechletServlet {
 
     public SimonSaysServlet() {
