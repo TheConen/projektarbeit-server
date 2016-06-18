68d9220d50956265d4ea0e128428a93444f98196
TheConen <theconen@gmail.com>
2016-06-18 18:00:50 +0200

    Registered CallForHelpServlet in web.xml

--- src/main/java/projektarbeit/server/CallForHelp/CallForHelpServlet.java
+++ src/main/java/projektarbeit/server/CallForHelp/CallForHelpServlet.java
@@ -2,8 +2,6 @@ package projektarbeit.server.CallForHelp;
 
 import com.amazon.speech.speechlet.servlet.SpeechletServlet;
 
-import projektarbeit.server.simonsays.SimonSaysSpeechlet;
-
 public class CallForHelpServlet extends SpeechletServlet {
     
 	public CallForHelpServlet() {
